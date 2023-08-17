e1e0059cdce4f5b2b2ff8bdd0312b58e367ec473
Sebastian Krüger <sk@honeymachine.io>
2023-08-17 12:59:56 +0200

    chore: cleanup

--- tests/test_amix.py
+++ tests/test_amix.py
@@ -15,34 +15,6 @@ __copyright__ = "Sebastian Krüger"
 __license__ = "MIT"
 
 
-def _sha1_checksum(data: (str, bytearray, bytes, io.BufferedReader, io.FileIO)) -> str:
-    """
-    create sha1 checksum
-    :param data: input data to check sha1 checksum
-    :type data: str, bytearray, bytes, io.BufferedReader, io.FileIO
-    :return: sha1 hash
-    :rtype: str
-    """
-    # byte
-    if isinstance(data, (bytes, bytearray)):
-        return hashlib.sha1(data).hexdigest()
-
-    # file
-    elif isinstance(data, str) and os.access(data, os.R_OK):
-        return hashlib.sha1(open(data, "rb").read()).hexdigest()
-
-    # file object
-    elif isinstance(data, (io.BufferedReader, io.FileIO)):
-        return hashlib.sha1(data.read()).hexdigest()
-
-    # string
-    elif isinstance(data, str):
-        return hashlib.sha1(data.encode()).hexdigest()
-
-    else:
-        raise ValueError("invalid input. input must be string, byte or file")
-
-
 def test_run(snapshot):
     """Test Amix().run"""
     fixtures = glob.glob(os.path.join(os.path.dirname(__file__), "fixtures", "*.yml"))
@@ -59,9 +31,12 @@ def test_run(snapshot):
         ).run()
 
         snapshot.assert_match(
-            _sha1_checksum(
-                os.path.join(os.path.dirname(__file__), "tmp", test_name + ".wav")
-            ),
+            hashlib.sha1(
+                open(
+                    os.path.join(os.path.dirname(__file__), "tmp", test_name + ".wav"),
+                    "rb",
+                ).read()
+            ).hexdigest(),
             os.path.join(snapshots_dir, test_name + ".wav.snapshot"),
         )
 
